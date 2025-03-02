388e5df3a4d0747f6aa3fdc96e56abaa1a49f026
matthewlouismueller <mattmueller93@gmail.com>
2025-03-02 18:57:50 +0000

    initial commit

---
+++ src/main.py
@@ -0,0 +1,101 @@
+import asyncio
+from typing import Any, ClassVar, Final, List, Mapping, Optional, Sequence
+
+from typing_extensions import Self
+from viam.components.sensor import Sensor
+from viam.module.module import Module
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.easy_resource import EasyResource
+from viam.resource.types import Model, ModelFamily
+from viam.utils import SensorReading, struct_to_dict
+
+import Adafruit_ADS1x15
+import time
+
+class adc(Sensor, EasyResource):
+    MODEL: ClassVar[Model] = Model(ModelFamily("kodama", "ads1115"), "adc")
+
+    @classmethod
+    def new(
+        cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
+        """This method creates a new instance of this Sensor component.
+        The default implementation sets the name from the `config` parameter and then calls `reconfigure`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+            dependencies (Mapping[ResourceName, ResourceBase]): The dependencies (both implicit and explicit)
+
+        Returns:
+            Self: The resource
+        """
+        return super().new(config, dependencies)
+
+    @classmethod
+    def validate_config(cls, config: ComponentConfig) -> Sequence[str]:
+        """This method allows you to validate the configuration object received from the machine,
+        as well as to return any implicit dependencies based on that `config`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+
+        Returns:
+            Sequence[str]: A list of implicit dependencies
+        """
+        fields = config.attributes.fields
+
+        if "i2c_address" in fields:
+            if not fields["i2c_address"].HasField("string"):
+                raise Exception("i2c_address must be a valid string.")
+            
+        if "i2c_bus" in fields:
+            if not fields["i2c_bus"].HasField("string"):
+                raise Exception("i2c_bus must be a valid string.")
+
+        return []
+
+    def reconfigure(
+        self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ):
+        """This method allows you to dynamically update your service when it receives a new `config` object.
+
+        Args:
+            config (ComponentConfig): The new configuration
+            dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
+        """
+        attrs = struct_to_dict(config.attributes)
+        self.i2c_address = attrs.get("i2c_address", "0x84")
+        self.i2c_bus = attrs.get("i2c_bus", "1")
+        return super().reconfigure(config, dependencies)
+
+    async def get_readings(
+        self,
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Mapping[str, SensorReading]:
+        
+        adc = Adafruit_ADS1x15.ADS1015(address=0x48, busnum=1)
+
+        chan0 = adc.read_adc(0, gain=1)
+        time.sleep(0.1)
+        chan1 = adc.read_adc(1, gain=1)
+        time.sleep(0.1)
+        chan2 = adc.read_adc(2, gain=1)
+        time.sleep(0.1)
+        chan3 = adc.read_adc(3, gain=1)
+
+        # Return a dictionary of the readings
+        return {
+            "chan0": chan0,
+            "chan1": chan1,
+            "chan2": chan2,
+            "chan3": chan3
+        }
+
+if __name__ == "__main__":
+    asyncio.run(Module.run_from_registry())
+
